3a7583afece9be1f0327878780c91c8a6141c247
Ruffy45 <74126164+Ruffy45@users.noreply.github.com>
2022-06-15 20:39:17 +0530

    Delete Portfolio Website directory

--- Portfolio Website/birthday.js
+++
@@ -1,8 +0,0 @@
-
-
-setInterval(() => {
-	let time = (new Date() - new Date(2002,10,5)) / (31557600000); // milliseconds per year
-	document.getElementById("age").innerHTML = time.toString().substring(0, 12);
-}, 50);
-
-
